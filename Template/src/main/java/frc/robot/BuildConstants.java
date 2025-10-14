cb266b215c5feec38fa51cb8e9900956838bb2d7
SlushEE0 <neeladem2009@gmail.com>
2025-10-13 19:13:45 -0700

    akit build constants

---
+++ Template/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Template";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 68;
+  public static final String GIT_SHA = "506ea996010595d67ca7f7906ac83a24169a636f";
+  public static final String GIT_DATE = "2025-10-04 16:56:16 PDT";
+  public static final String GIT_BRANCH = "revamp";
+  public static final String BUILD_DATE = "2025-10-13 19:08:08 PDT";
+  public static final long BUILD_UNIX_TIME = 1760407688496L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
